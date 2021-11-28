a066f78f0f0af3c1407b3cb723a2cc30d89a8793
Mukul <mukulkumar627@gmail.com>
2021-11-28 22:54:09 +0530

    Add routes for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LoginPage from './Components/LoginPage/LoginPage';
+import Dashboard from './Components/Dashboard/Dashboard';
+import SignupPage from './Components/SignupPage/SignupPage';
 
 function App() {
+
+  function requireAuth(nextState, replace, next) {
+    const authenticated = localStorage.getItem('user');
+    if (!authenticated) {
+      replace({
+        pathname: "/login",
+        state: { nextPathname: nextState.location.pathname }
+      });
+    }
+    next();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/signup' element={<SignupPage/>}/>
+        <Route path='/dashboard' element={<Dashboard/>} onEnter={requireAuth}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
